628818e8d642224c765d6d5e2dae86f5c175c69b
hawoline <neltanov02@gmail.com>
2020-05-31 17:31:13 +0800

    Initial commit

---
+++ src/ru/hawoline/neuralnetwork/NeuralNetTest.java
@@ -0,0 +1,9 @@
+package ru.hawoline.neuralnetwork;
+
+public class NeuralNetTest {
+	public static void main(String[] args) {
+		NeuralNet n = new NeuralNet();
+		n.initNet();
+		n.printNet();
+	}
+}
